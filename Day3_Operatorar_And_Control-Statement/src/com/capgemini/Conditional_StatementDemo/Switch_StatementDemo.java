0017a829e6c96873ba217c2b03a4f274387fea37
vaish <vaish@Dharmendra>
2022-11-20 19:39:50 +0530

    First Commit

--- Day3_Operatorar_And_Control-Statement/src/com/capgemini/Conditional_StatementDemo/Switch_StatementDemo.java
+++ Day3_Operatorar_And_Control-Statement/src/com/capgemini/Conditional_StatementDemo/Switch_StatementDemo.java
@@ -4,46 +4,48 @@ import java.util.Scanner;
 
 public class Switch_StatementDemo {
 	public static void main(String args[]) {
+		System.out.println("Enter Two Number :");
 		int a,b,c,ch;
 		@SuppressWarnings("resource")
 		Scanner s=new Scanner(System.in);
 		a=s.nextInt();
 		b=s.nextInt();
-		System.out.println("Enter Your Choice");
+		System.out.println("Enter Your Choice :");
 		ch=s.nextInt();
 switch(ch)
 {
 case 1 : c=a+b;
 
-	System.out.println("Addition of Number"+c);
+	System.out.println("Addition of Number :"+c);
 	break;
 
 case 2 : c=a-b;
 
-	System.out.println("Substraction of Number"+c);
+	System.out.println("Substraction of Number :"+c);
 	break;
 
 case 3 : c=a*b;
 
-	System.out.println("Multiplicatin of Number"+c);
+	System.out.println("Multiplicatin of Number :"+c);
 	break;
 
 
 case 4 : c=(a*b);
 
-	System.out.println("Division of Number"+c);
+	System.out.println("Division of Number :"+c);
 	break;
 
 case 5 : c=a%b;
 
-	System.out.println("Modulus of Number"+c);
+	System.out.println("Modulus of Number :"+c);
 	break;
 
 
 default:
 
-	System.out.println("Invalid Choice");
+	System.out.println("Invalid Choice!!");
 
 }
 }
+
 }
