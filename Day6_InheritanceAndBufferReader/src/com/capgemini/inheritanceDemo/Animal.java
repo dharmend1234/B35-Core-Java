6b00468a57bd7668c705c0c3537a501fb313fcdc
vaish <vaish@Dharmendra>
2022-11-25 02:09:43 +0530

    First Commit

---
+++ Day6_InheritanceAndBufferReader/src/com/capgemini/inheritanceDemo/Animal.java
@@ -0,0 +1,12 @@
+package com.capgemini.inheritanceDemo;
+//Parent Class or Superclass
+//Demo on Single Inheritance
+public class Animal {
+	protected String name;
+	public void display() 
+	{
+		System.out.println("Enter the name of the animal is "+name);
+	}
+	
+
+}
