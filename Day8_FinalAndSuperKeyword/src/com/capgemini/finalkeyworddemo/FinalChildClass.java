39377f00e558bd080f026ea529b67cb5276d4eac
vaish <vaish@Dharmendra>
2022-11-30 03:54:00 +0530

    First Commit

---
+++ Day8_FinalAndSuperKeyword/src/com/capgemini/finalkeyworddemo/FinalChildClass.java
@@ -0,0 +1,15 @@
+package com.capgemini.finalkeyworddemo;
+
+//child class
+public class FinalChildClass extends FinalParentClass{
+	
+	//method
+	public void print()
+	{
+		System.out.println(name);
+	}
+	
+	
+	
+
+}
---
+++ Day8_FinalAndSuperKeyword/src/com/capgemini/finalkeyworddemo/FinalMethodExecutor.java
@@ -0,0 +1,16 @@
+package com.capgemini.finalkeyworddemo;
+
+public class FinalMethodExecutor {
+
+	public static void main(String[] args) {
+		FinalMethodWithChild f=new FinalMethodWithChild ();
+		f.aadhar_no=345678991678L;
+		f.Pan_no="DFK157890";
+		//f.print();
+		f.print(f.Pan_no);
+		
+
+	}
+
+}
+
