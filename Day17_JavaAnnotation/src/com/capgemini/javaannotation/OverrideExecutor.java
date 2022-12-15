c75af218b4b2e05b831deae18343e3b713edd37f
vaish <vaish@Dharmendra>
2022-12-16 02:47:04 +0530

    First Commit

---
+++ Day17_JavaAnnotation/src/com/capgemini/javaannotation/OverrideExecutor.java
@@ -0,0 +1,12 @@
+package com.capgemini.javaannotation;
+//driver class
+public class OverrideExecutor {
+
+	public static void main(String[] args) {
+		Child c=new Child();
+		c.display();
+		
+
+	}
+
+}
---
+++ Day17_JavaAnnotation/src/com/capgemini/javaannotation/Parent.java
@@ -0,0 +1,14 @@
+package com.capgemini.javaannotation;
+//Super class
+public class Parent {
+	
+	public String name="Nita R";
+	public void display()
+	{
+		System.out.println("Parent Name: "+name);
+	}
+	
+	
+	
+
+}
