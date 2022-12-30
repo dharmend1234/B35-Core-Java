4b12c7bc977452aa09f20cfe74ad40f86e73887a
vaish <vaish@Dharmendra>
2022-12-30 21:34:09 +0530

    First Commit

---
+++ Day-20_JDBC_CRUD/src/com/capgemini/repository/EmployeeRepo.java
@@ -0,0 +1,20 @@
+package com.capgemini.repository;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.capgemini.entities.Employee;
+
+public interface EmployeeRepo {
+	
+	//to execute a query use Statement
+	Statement createStatement();
+	PreparedStatement createPrepareStatement(String query);
+	public ResultSet retreive(String query);
+	public int add(String query, Employee emp);
+	public int update(String query, Employee emp);
+	public int delete(String query);
+
+
+}
